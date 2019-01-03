9d27fe23bcff8f7b774ae9d53cd4ff9a94509621
Dhyan <dhyanpp@gmail.com>
2019-01-03 02:52:57 -0600

    UUID generation, JSON storage, basic site templates

---
+++ app.py
@@ -0,0 +1,36 @@
+import uuid, json
+from flask import Flask, render_template, redirect, url_for, request
+from flask_uuid import FlaskUUID
+
+app = Flask(__name__)
+FlaskUUID(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/create_uuid/')
+def create_uuid():
+    with open('./static/urls.json', 'r+') as f:
+        data = json.load(f)
+        hashcode = str(uuid.uuid4())
+        data[hashcode] = 0
+        f.seek(0)
+        json.dump(data, f, indent=4)
+        f.truncate()
+        return redirect(url_for('statistics', hashcode=uuid.uuid4()))
+
+
+@app.route('/tracky/<uuid(strict=False):hashcode>/counter/')
+def counter(hashcode):
+    return repr(hashcode)
+
+
+@app.route('/tracky/<uuid(strict=False):hashcode>/statistics/')
+def statistics(hashcode):
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
